a6f1f174bd14f5c24b1a6019adde4d6ead438571
018bf <>
2025-08-03 22:28:11 +0200

    add gitignore

--- internal/pkg/grpc/config.go
+++
@@ -1,5 +0,0 @@
-package grpc
-
-type Config struct {
-	Address string `toml:"address"`
-}
--- internal/pkg/postgres/migrations/000003_tags.up.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE public.tags
-(
-    id          uuid                  DEFAULT uuid_generate_v4()
-        CONSTRAINT tags_pk PRIMARY KEY,
-    post_id uuid NOT NULL,
-    value text NOT NULL,
-    updated_at  timestamp    NOT NULL DEFAULT (now() at time zone 'utc'),
-    created_at  timestamp    NOT NULL DEFAULT (now() at time zone 'utc')
-);
